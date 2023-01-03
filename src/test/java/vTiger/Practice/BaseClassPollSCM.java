8228f8258ed5386965a44416093a9279280a43f3
alwin <alwin@Aney>
2023-01-03 19:20:11 +0530

    commit

---
+++ src/test/java/vTiger/Practice/BaseClassPollSCM.java
@@ -0,0 +1,18 @@
+package vTiger.Practice;
+
+import org.testng.annotations.Test;
+
+import vTiger.GenericUtilities.BaseClass;
+
+public class BaseClassPollSCM  extends BaseClass{
+
+		@Test
+		public void test()
+		{
+			System.out.println("hi");
+			
+			
+			System.out.println("hi");
+			
+		}
+}
